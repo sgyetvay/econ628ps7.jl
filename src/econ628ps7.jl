a6b5f7977f17360f56afb41e204acfb9a781f766
Sam Gyetvay <sam.gyetvay@gmail.com>
2018-11-21 16:12:53 -0800

    hello

--- src/econ628ps7.jl
+++ src/econ628ps7.jl
@@ -1,5 +1,32 @@
 module econ628ps7
 
-greet() = print("Hello World!")
+using ForwaDiff, LinearAlgebra
 
-end # module
+function newton(f, f_prime, x_0; tolerance = 1E-7, maxiter = 1000)
+    #Setup the algorithm
+    x_old = x_0
+    error = Inf
+    iter = 1
+    while error > tolerance && iter <= maxiter
+        x_new = x_old - (f(x_old)/f_prime(x_old))
+        error = sqrt((x_new - x_old)^2)
+        x_old = x_new
+        iter = iter + 1
+    end
+    if iter == maxiter + 1
+        return nothing
+    else
+        return x_old
+end
+end
+
+f(x) = (x-1)^3
+f_prime(x) = 3*(x-1)^2
+
+function newton(f, x_0; tolerance = 1E-7, maxiter = 1000)
+f_prime = x -> ForwardDiff.derivative(f, x)
+return newton(f,f_prime,x_0;tolerance = tolerance, maxiter = maxiter)
+end
+
+export newton, f, f_prime
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using econ628ps7
+using econ627ps7
 using Test
+g(x)=(x-1.0)^2
+gprime(x)=2*(x-1)
 
-@testset "econ628ps7.jl" begin
-    # Write your own tests here.
-end
+@testset "Basic Newtons Method Tests" begin
+    @test newton(g,gprime,10.0) ≈ 1.0  atol=0.00001
+    @test newton(g,10)≈ 1.0 atol=0.00001
+end;
+
+@testset "Convergence" begin
+    @test (newton(x->x^2+1,x₀=10)==nothing)
+    @test (newton(x->x^2+1,x->2*x,x₀=10)==nothing)
+end;
+
+@testset "Max Iter" begin
+    @test (newton(g,x₀=10,maxiter=5)==nothing)
+    @test (newton(g,gprime,x₀=10,maxiter=5)==nothing)
+end;
+
+@testset "Tolerance" begin
+    x1=(newton(g,x₀=10,tol=1E-6))
+    x2=(newton(g,x₀=10,tol=1E-5))
+    @test(abs(x1-1)<abs(x2-1))
+end;
+
+@testset "BigFloat" begin
+    @test (newton(g,gprime,x₀=BigFloat(10))≈1.0)
+    @test (newton(g,x₀=BigFloat(10))≈1.0)
+end;
